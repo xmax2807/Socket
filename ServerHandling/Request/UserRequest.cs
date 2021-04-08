72118f57803411ce27eca83bbd823f4b96cf87dd
risethesummer <honhatlinhmail@gmail.com>
2021-04-08 14:06:49 +0700

    asd

---
+++ ServerHandling/Request/UserRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CommonResource
+{
+    [Serializable]
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public User(string userName, string password)
+        {
+            this.UserName = userName;
+            this.Password = password;
+        }
+    }
+
+    public enum TypeOfRequest
+    {
+        SIGN_UP, SIGN_IN
+    }
+    
+}
